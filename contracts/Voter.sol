52f0c4305cc023211864f8f6cff1106640ad9b95
byarustev <byarus45@gmail.com>
2019-08-20 10:38:26 +0300

    add voting by position name

--- contracts/Voter.sol
+++ contracts/Voter.sol
@@ -4,15 +4,40 @@ pragma experimental ABIEncoderV2;
 contract Voter{
     string[] options;
     uint[] votes;
+    struct OptionPos{
+        uint pos;
+        bool exists;
+    }
 
-    constructor (string[] _options ) public{
+    mapping(address=>bool) hasVoted;
+    mapping(string=>OptionPos) posOfOption;
+    
+    constructor (string[] memory _options ) public{
         options = _options;
         votes.length = options.length;
+        
+        //filling the mapping
+        for (uint i=0; i<options.length; i++){
+            OptionPos memory tempOptPos = OptionPos(i,true);
+            string memory optionName=options[i];
+            posOfOption[optionName]=tempOptPos;
+        }
+        
     }
 
     function vote(uint option) public {
         require(0<=option && option<options.length, "invalid vote");
+        require(!hasVoted[msg.sender],"you have already voted");
         votes[option] = votes[option]+1;
+        hasVoted[msg.sender]=true;
+    }
+
+    function vote(string memory posName) public{
+        require(!hasVoted[msg.sender],"you have already voted");
+        OptionPos memory tempOptPos= posOfOption[posName];
+        require(tempOptPos.exists,"option does not exist");
+        votes[tempOptPos.pos] = votes[tempOptPos.pos]+1;
+        hasVoted[msg.sender]=true;
     }
 
     function getOptions() public view returns (string[] memory){
